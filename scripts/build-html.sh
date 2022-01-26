3fd0a5e3685eb0158d07d8c54e0acc571bc64456
Tobias Looker <tobias.looker@mattr.global>
2022-01-26 13:42:53 +1300

    remove redundant workflows

---
+++ scripts/build-html.sh
@@ -0,0 +1,8 @@
+# Convert markdown to XML and HTML versions
+docker run -v `pwd`:/data danielfett/markdown2rfc draft-bbs-signatures.md || exit 1
+
+# Delete XML version
+#rm bbs-signatures-00.xml
+
+# Rename the HTML version for hosting with GH pages
+#mv bbs-signatures-00.html index.html
