2c38f011e8d06d0bb9b74f8ae45022d83270f017
Vasilis Kalos <kalosbasileios@gmail.com>
2023-01-20 18:49:32 +0200

    typo fix

--- tooling/keygen/src/main.rs
+++ tooling/keygen/src/main.rs
@@ -35,7 +35,6 @@ enum OutputType {
 
 impl std::str::FromStr for OutputType {
     type Err = String;
-
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
             "f" | "fi" | "fil" | "file" => Ok(OutputType::File),
@@ -44,6 +43,7 @@ impl std::str::FromStr for OutputType {
         }
     }
 }
+
 #[derive(StructOpt, Debug)]
 struct Opt {
     #[structopt(long, default_value = &IKM)]
@@ -52,7 +52,7 @@ struct Opt {
     key_info: String,
     #[structopt(short, long, default_value = "Print")]
     out: OutputType,
-    #[structopt(short, long)] //default_value = "../fixtures/fixture_data/keyPair.json"
+    #[structopt(short, long)]
     file: Option<String>,
 }
 
@@ -129,7 +129,7 @@ fn main() {
     }
 }
 
-// wright to file
+// write to file
 fn write_keypair_to_file(ikm: &str, key_info: Option<&str>, key_pair: KeyPair, file: Option<String>)
 {
     println!("writhing to file...");
